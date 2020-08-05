55b24d5834b4ce376a4c724d9a9865d554bc9ed5
Melvin Sebastian <48371867+melvin97n@users.noreply.github.com>
2020-08-06 00:36:07 +0530

    Update mask.py

--- lungmask/mask.py
+++ lungmask/mask.py
@@ -13,7 +13,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 warnings.filterwarnings("ignore", category=UserWarning)
 
 # stores urls and number of classes of the models
-model_urls = {('unet', 'R231'): ('https://github.com/JoHof/lungmask/releases/download/v0.0/unet_r231-d5d2fc3d.pth', 3),
+model_urls = {('unet', 'R231'): ('../input/unet-r231/unet_r231-d5d2fc3d.pth', 3),
               ('unet', 'LTRCLobes'): (
                   'https://github.com/JoHof/lungmask/releases/download/v0.0/unet_ltrclobes-3a07043d.pth', 6),
               ('unet', 'R231CovidWeb'): (
